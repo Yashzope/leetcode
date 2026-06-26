5c82f36e4f45ab0fdb7be173682c0a4747ddbbc4
Yash zope <98435206+Yashzope@users.noreply.github.com>
2026-06-26 09:41:40 +0530

    Time: 1175 ms (93.96%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+-- select 
+--    p.product_name,
+--    s.year,
+--    s.price 
+--    from Sales s left join Product p 
+--    on p.product_id = s.product_id 
+--    join Product p on s.product_id=p.product_name 
+-- group by sale_id;
+
+
+select p.product_name,
+   s.year,
+   s.price 
+ from Sales s
+left join Product p
+on s.product_id = p.product_id;
